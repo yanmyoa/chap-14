3b0e786b291224443f858d69f2f497c5b9cc0200
YanMyoAung <yanmyoaung.mdy1500@gmail.com>
2022-08-20 22:14:37 +0630

    Real-Time Firestore Listener(fetch all messages and show messages

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,23 +1,32 @@
 <template>
   <div class="chat-window">
-    <div class="messages">
+    <div class="messages" v-for="message in messages" :key="message.id">
         <div class="single">
-            <span class="created-at">3mins ago</span>
-            <span class="name">hmt</span>
-            <span class="message">hi there man</span>
-        </div>
-        <div class="single">
-            <span class="created-at">5mins ago</span>
-            <span class="name">aung aung</span>
-            <span class="message">hi there man</span>
+            <span class="created-at">{{message.created_at}}</span>
+            <span class="name">{{message.name}}</span>
+            <span class="message">{{message.message}}</span>
         </div>
     </div>
   </div>
 </template>
 
 <script>
+import { db } from '@/firebase/config'
+import { ref } from '@vue/reactivity'
 export default {
+    setup(){
+        let messages=ref([]);
 
+        db.collection("messages").orderBy("created_at").onSnapshot((snap)=>{
+            let results=[];
+            snap.docs.forEach((doc)=>{
+                let document={...doc.data(),id:doc.id}
+                results.push(document);
+            })
+            messages.value=results;
+        })
+        return {messages};
+    }
 }
 </script>
 
